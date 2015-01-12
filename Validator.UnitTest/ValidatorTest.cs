d86f54fef501d4e9ec8473eb109301b154a70358
ByteBlast <byteblast@gmail.com>
2015-01-12 12:19:54 +0000

    Added IsAlpha

---
+++ Validator.UnitTest/ValidatorTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Xunit.Extensions;
+
+namespace Validator.UnitTest
+{
+    public class ValidatorTest
+    {
+        [Theory]
+        [InlineData("Foo", true)]
+        [InlineData("1Foo", false)]
+        [InlineData("123", false)]
+        [InlineData("1Foo\r\n12", false)]
+        public void IsAlpha_ReturnsCorrectResult(string input, bool expected)
+        {
+            var actual = Validator.IsAlpha(input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Validator/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Validator
+{
+    public class Validator
+    {
+        public static bool IsAlpha(string input)
+        {
+            return Regex.IsMatch(input, "^[A-z]+$");
+        }
+    }
+}
