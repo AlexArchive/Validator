7eab503cb0d6b84be3bd7c8577d7b6f708eeff93
ByteBlast <byteblast@gmail.com>
2015-01-21 23:44:50 +0000

    Added IsIsbn version 10

--- Validator.UnitTest/ValidatorTest.cs
+++ Validator.UnitTest/ValidatorTest.cs
@@ -258,5 +258,24 @@ namespace Validator.UnitTest
             var actual = Validator.IsNull(input);
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("0596004427", true)]
+        [InlineData("0-596-00442-7", true)]
+        [InlineData("0 596 00442 7", true)]
+        [InlineData("161729134X", true)]
+        [InlineData("1-617291-34-X", true)]
+        [InlineData("1 617291 34 X", true)]
+        [InlineData("3423214", false)]
+        [InlineData("342321412122", false)]
+        [InlineData("3423214121", false)]
+        [InlineData("3-423-21412-1", false)]
+        [InlineData("3 423 21412 1", false)]
+        [InlineData("Foo", false)]
+        public void IsIsbnVersion10(string input, bool expected)
+        {
+            var actual = Validator.IsIsbn(input);
+            Assert.Equal(expected, actual);
+        }
     }
 }
