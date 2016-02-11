ac2fea594dfdce0d7fa7afa4ab0c9c35783b8ce0
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-02-10 22:26:04 -0500

    fixing rest of unit tests. IsUrl passing on "abc@xyz.com" still needs to be figured out. From what I can tell from the code and the comments, "abc@xyz.com" shoud fail, but it doesn't. As a result, the entire IsUrl unit test is skipped, even though the rest of the InlineData has value.

--- Validator.UnitTest/ValidatorTest.cs
+++ Validator.UnitTest/ValidatorTest.cs
@@ -8,95 +8,95 @@ namespace Validator.UnitTest
 {
     public class ValidatorTest
     {
-        //[Theory]
-        //[InlineData("9783836221191", true)]
-        //[InlineData("978-3-8362-2119-1", true)]
-        //[InlineData("978 3 8362 2119 1", true)]
-        //[InlineData("9784873113685", true)]
-        //[InlineData("978-4-87311-368-5", true)]
-        //[InlineData("978 4 87311 368 5", true)]
-        //[InlineData("9783836221190", false)]
-        //[InlineData("978-3-8362-2119-0", false)]
-        //[InlineData("978 3 8362 2119 0", false)]
-        //[InlineData("Foo", false)]
-        //public void IsIsbnVersion13(string input, bool expected)
-        //{
-        //    var actual = input.IsIsbn(IsbnVersion.Thirteen);
-        //    Assert.Equal(expected, actual);
-        //}
+        [Theory]
+        [InlineData("9783836221191", true)]
+        [InlineData("978-3-8362-2119-1", true)]
+        [InlineData("978 3 8362 2119 1", true)]
+        [InlineData("9784873113685", true)]
+        [InlineData("978-4-87311-368-5", true)]
+        [InlineData("978 4 87311 368 5", true)]
+        [InlineData("9783836221190", false)]
+        [InlineData("978-3-8362-2119-0", false)]
+        [InlineData("978 3 8362 2119 0", false)]
+        [InlineData("Foo", false)]
+        public void IsIsbnVersion13(string input, bool expected)
+        {
+            var actual = input.IsIsbn(IsbnVersion.Thirteen);
+            Assert.Equal(expected, actual);
+        }
 
-        //[Fact]
-        //public void IsIsbnnThrowsWhenSuppliedUnknownVersion()
-        //{
-        //    const int version = 42;
-        //    var message = Assert.Throws<ArgumentOutOfRangeException>(() => 
-        //    input.IsIsbn("9784873113685", (IsbnVersion)version));
-        //    Assert.Contains("Isbn version " + version + " is not supported.", 
-        //        message.Message);
-        //}
+        [Theory]
+        [InlineData("9784873113685")]
+        public void IsIsbnnThrowsWhenSuppliedUnknownVersion(string input)
+        {
+            const int version = 42;
+            var message = Assert.Throws<ArgumentOutOfRangeException>(() => input.IsIsbn((IsbnVersion)version));
+            Assert.Contains("Isbn version " + version + " is not supported.", message.Message);
+        }
 
-        //[Theory]
-        //[InlineData(UuidVersion.Three, "A987FBC9-4BED-3078-CF07-9141BA07C9F3", true)]
+        [Theory]
+        [InlineData(UuidVersion.Three, "A987FBC9-4BED-3078-CF07-9141BA07C9F3", true)]
 
-        //[InlineData(UuidVersion.Three, "", false)]
-        //[InlineData(UuidVersion.Three, "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
-        //[InlineData(UuidVersion.Three, "934859", false)]
-        //[InlineData(UuidVersion.Three, "AAAAAAAA-1111-1111-AAAG-111111111111", false)]
-        //[InlineData(UuidVersion.Three, "A987FBC9-4BED-4078-8F07-9141BA07C9F3", false)]
-        //[InlineData(UuidVersion.Three, "A987FBC9-4BED-5078-AF07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Three, "", false)]
+        [InlineData(UuidVersion.Three, "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Three, "934859", false)]
+        [InlineData(UuidVersion.Three, "AAAAAAAA-1111-1111-AAAG-111111111111", false)]
+        [InlineData(UuidVersion.Three, "A987FBC9-4BED-4078-8F07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Three, "A987FBC9-4BED-5078-AF07-9141BA07C9F3", false)]
 
-        //[InlineData(UuidVersion.Four, "713ae7e3-cb32-45f9-adcb-7c4fa86b90c1", true)]
-        //[InlineData(UuidVersion.Four, "625e63f3-58f5-40b7-83a1-a72ad31acffb", true)]
-        //[InlineData(UuidVersion.Four, "57b73598-8764-4ad0-a76a-679bb6640eb1", true)]
-        //[InlineData(UuidVersion.Four, "9c858901-8a57-4791-81fe-4c455b099bc9", true)]
+        [InlineData(UuidVersion.Four, "713ae7e3-cb32-45f9-adcb-7c4fa86b90c1", true)]
+        [InlineData(UuidVersion.Four, "625e63f3-58f5-40b7-83a1-a72ad31acffb", true)]
+        [InlineData(UuidVersion.Four, "57b73598-8764-4ad0-a76a-679bb6640eb1", true)]
+        [InlineData(UuidVersion.Four, "9c858901-8a57-4791-81fe-4c455b099bc9", true)]
 
-        //[InlineData(UuidVersion.Four, "", false)]
-        //[InlineData(UuidVersion.Four, "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
-        //[InlineData(UuidVersion.Four, "934859", false)]
-        //[InlineData(UuidVersion.Four, "AAAAAAAA-1111-1111-AAAG-111111111111", false)]
-        //[InlineData(UuidVersion.Four, "A987FBC9-4BED-5078-AF07-9141BA07C9F3", false)]
-        //[InlineData(UuidVersion.Four, "A987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Four, "", false)]
+        [InlineData(UuidVersion.Four, "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Four, "934859", false)]
+        [InlineData(UuidVersion.Four, "AAAAAAAA-1111-1111-AAAG-111111111111", false)]
+        [InlineData(UuidVersion.Four, "A987FBC9-4BED-5078-AF07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Four, "A987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
 
-        //[InlineData(UuidVersion.Five, "987FBC97-4BED-5078-AF07-9141BA07C9F3", true)]
-        //[InlineData(UuidVersion.Five, "987FBC97-4BED-5078-BF07-9141BA07C9F3", true)]
-        //[InlineData(UuidVersion.Five, "987FBC97-4BED-5078-8F07-9141BA07C9F3", true)]
-        //[InlineData(UuidVersion.Five, "987FBC97-4BED-5078-9F07-9141BA07C9F3", true)]
+        [InlineData(UuidVersion.Five, "987FBC97-4BED-5078-AF07-9141BA07C9F3", true)]
+        [InlineData(UuidVersion.Five, "987FBC97-4BED-5078-BF07-9141BA07C9F3", true)]
+        [InlineData(UuidVersion.Five, "987FBC97-4BED-5078-8F07-9141BA07C9F3", true)]
+        [InlineData(UuidVersion.Five, "987FBC97-4BED-5078-9F07-9141BA07C9F3", true)]
 
-        //[InlineData(UuidVersion.Five, "", false)]
-        //[InlineData(UuidVersion.Five, "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
-        //[InlineData(UuidVersion.Five, "934859", false)]
-        //[InlineData(UuidVersion.Five, "AAAAAAAA-1111-1111-AAAG-111111111111", false)]
-        //[InlineData(UuidVersion.Five, "9c858901-8a57-4791-81fe-4c455b099bc9", false)]
-        //[InlineData(UuidVersion.Five, "A987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
-        //public void IsUuidWithVersion(UuidVersion version, string input, bool expectedValid)
-        //{
-        //    var actual = input.IsUuid(version);
-        //    Assert.Equal(actual, expectedValid);
-        //}
+        [InlineData(UuidVersion.Five, "", false)]
+        [InlineData(UuidVersion.Five, "xxxA987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
+        [InlineData(UuidVersion.Five, "934859", false)]
+        [InlineData(UuidVersion.Five, "AAAAAAAA-1111-1111-AAAG-111111111111", false)]
+        [InlineData(UuidVersion.Five, "9c858901-8a57-4791-81fe-4c455b099bc9", false)]
+        [InlineData(UuidVersion.Five, "A987FBC9-4BED-3078-CF07-9141BA07C9F3", false)]
+        public void IsUuidWithVersion(UuidVersion version, string input, bool expectedValid)
+        {
+            var actual = input.IsUuid(version);
+            Assert.Equal(actual, expectedValid);
+        }
 
-        //[Fact]
-        //public void IsUuidThrowsWhenSuppliedUnknownVersion()
-        //{
-        //    const int invalidVersion = 99;
+        [Theory]
+        [InlineData("A987FBC9-4BED-3078-CF07-9141BA07C9F3")]
+        public void IsUuidThrowsWhenSuppliedUnknownVersion(string input)
+        {
+            const int invalidVersion = 99;
+            var message = Assert.Throws<ArgumentOutOfRangeException>(() => input.IsUuid((UuidVersion)invalidVersion));
+            Assert.Contains("Uuid version " + invalidVersion + " is not supported.", message.Message);
+        }
 
-        //    var message = Assert.Throws<ArgumentOutOfRangeException>(() =>
-        //    input.IsUuid("A987FBC9-4BED-3078-CF07-9141BA07C9F3", (UuidVersion)invalidVersion));
-        //    Assert.Contains("Uuid version " + invalidVersion + " is not supported.",
-        //        message.Message);
-        //}
+        [Theory]
+        [InlineData("example.com.")]
+        public void IsFqdnWithTrailingDotOption(string input)
+        {
+            var actual = input.IsFqdn(allowTrailingDot: true);
+            Assert.True(actual);
+        }
 
-        //[Fact]
-        //public void IsFqdnWithTrailingDotOption()
-        //{
-        //    var actual = input.IsFqdn("example.com.", allowTrailingDot: true);
-        //    Assert.True(actual);
-        //}
-        //[Fact]
-        //public void IsFqdnWithUnderscoreOption()
-        //{
-        //    var actual = input.IsFqdn("test_.com", allowUnderscore: true);
-        //    Assert.True(actual);
-        //}
+        [Theory]
+        [InlineData("test_.com")]
+        public void IsFqdnWithUnderscoreOption(string input)
+        {
+            var actual = input.IsFqdn(allowUnderscore: true);
+            Assert.True(actual);
+        }
 
         [Theory]
         [InlineData("Foo", true)]
