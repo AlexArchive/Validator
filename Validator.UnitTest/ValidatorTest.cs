23d1d30b9255c5f96d69bcddb4c240d0663a6ea5
saidfagan <developer.fagan@gmail.com>
2020-09-17 22:08:25 +0400

    Ported isMacAddress method

--- Validator.UnitTest/ValidatorTest.cs
+++ Validator.UnitTest/ValidatorTest.cs
@@ -843,5 +843,25 @@ namespace Validator.UnitTest
             var actual = Validator.IsMobilePhone(input, locale);
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("aa:aa:aa:aa:aa:aa", true)]
+        [InlineData("1a:2b:3c:4d:5e:6f", true)]
+        [InlineData("00:00:00:00:00:00", true)]
+        [InlineData("00 00 00 00 00 00", true)]
+        [InlineData("AA-11-BB-22-CC-33", true)]
+        [InlineData("ABCD.1234.5678", true)]
+        [InlineData("qwertyuiop", false)]
+        [InlineData("zz:zz:yy:yy:xx:xx", false)]
+        [InlineData("a1@b2@c3@d4@e5@6f", false)]
+        [InlineData("aa:aa:aa:aa:aa", false)]
+        [InlineData("00:00-00:00-00:00", false)]
+        [InlineData("AA-11-BZ-22-CC-33", false)]
+        [InlineData("AA 11 BB 22 CC33", false)]
+        public void IsMacAddress(string input, bool expected)
+        {
+            var actual = Validator.IsMacAddress(input);
+            Assert.Equal(expected, actual);
+        }
     }
 }
